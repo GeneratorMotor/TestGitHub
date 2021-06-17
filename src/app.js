08081163c5286656bf3e2bc0829613456c6dc30d
Evgeny Dugin <t3generator@yandex.ru>
2021-06-17 15:09:41 +0400

    Добавили новый класс js
    *Описывает пользователя
    *Добавлены функции к классу

--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
 function helloGit(){
   alert("Hello GitHub");
 }
+
+let user = {
+  age : 45,
+  name : "Оля",
+  ["Место работы"] : "Нита фарм",
+  getName : function(){
+    return this.name;
+  },
+  setName : function(name){
+    if(isNaN(name)== false){
+      if(name != undefined || name != null )
+      {
+      this.name = name;
+      }
+    }
+    else{
+      alert("Имя не может быть числом");
+    }
+  }
+}
